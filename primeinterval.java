4cb55b865e835fd0a5013c6121192b67cb14c54d
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-27 20:06:47 +0530

    Create primeinterval.java

---
+++ primeinterval.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int s1, s2, s3, flag = 0, i, j;
+         Scanner s = new Scanner(System.in);
+         s1 = s.nextInt();
+         s2 = s.nextInt();
+            for(i = s1; i <= s2; i++)
+         {
+             for( j = 2; j < i; j++)
+             {
+                 if(i % j == 0)
+                 {
+                     flag = 0;
+                     break;
+                 }
+                 else
+                 {
+                     flag = 1;
+                 }
+             }
+             if(flag == 1)
+             {
+                 System.out.println(i);
+             }
+         }
+	}
+}
