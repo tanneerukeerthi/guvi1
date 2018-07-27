14d13685a5bb181c51c083c46220b7ced0cc13df
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-27 14:04:43 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int base, exponent;
+        long result = 1;
+        Scanner sc=new Scanner(System.in);
+        base=sc.nextInt();
+        exponent=sc.nextInt();
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println(result);
+    }
+
+
+}
