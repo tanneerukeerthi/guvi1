a1b4e8460784967f116cd5332e3c06f0412b21ea
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-27 15:15:37 +0530

    Create eveninterval.java

---
+++ eveninterval.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int num,m,n,j;
+	  Scanner sc=new Scanner(System.in);
+	  m=sc.nextInt();
+	  n=sc.nextInt();
+	 num=m;
+	  while(num<n)
+	  {
+	     if(num==m)
+	     {
+	     num=num+1;
+	     }
+	  	
+	     if(num%2==0)
+	  System.out.print(num+"\t");
+	  num++;
+	}
+	}	
+}
