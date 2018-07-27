ae09b790ca412accff769b266ac70973f6ad0e0e
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-27 14:31:04 +0530

    Create interval.java

---
+++ interval.java
@@ -0,0 +1,18 @@
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int num,m,n;
+	  Scanner sc=new Scanner(System.in);
+	  m=sc.nextInt();
+	  n=sc.nextInt();
+	  for(num=m;num<n;num++)
+	  {
+	  	if(num%2==1)
+	  	{
+	  		System.out.println(num);
+	  	}
+	  }
+	}
+}
