68f87dd56e518860a057d96797931491756f2cc0
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-27 20:29:23 +0530

    Create amstronginterval.java

---
+++ amstronginterval.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int low, high;
+		 Scanner sc=new Scanner(System.in);
+		 low=sc.nextInt();
+		 high=sc.nextInt();
+
+        for(int number = low + 1; number < high; ++number) {
+            int digits = 0;
+            int result = 0;
+            int originalNumber = number;
+
+            // number of digits calculation
+            while (originalNumber != 0) {
+                originalNumber /= 10;
+                ++digits;
+            }
+
+            originalNumber = number;
+
+            // result contains sum of nth power of its digits
+            while (originalNumber != 0) {
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digits);
+                originalNumber /= 10;
+            }
+
+            if (result == number)
+                System.out.print(number + " ");
+        }       
+	}
+}
