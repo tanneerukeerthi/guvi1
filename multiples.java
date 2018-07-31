f5ec584d6a5fabac2e59cee55ec8ffaefb701367
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-07-31 14:44:18 +0530

    Create multiples.java

---
+++ multiples.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,d;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=1;i<=5;i++)
+		{
+			d=n*i;
+			System.out.print(d+" ");
+		}
+	}
+}
