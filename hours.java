92dbd87d677243ca9a99bddceb6a54b1f569c364
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-02 20:22:39 +0530

    Create hours.java

---
+++ hours.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,hour,min;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n<60)
+		{
+		System.out.println(0+" "+n);
+		}
+		else
+		{
+		hour=(int)n/60;
+		min=(int)n%60;
+		System.out.println(hour+" "+min);
+		}
+	}
+}
