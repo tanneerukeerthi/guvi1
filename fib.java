a900eb9e3621e41def06b4d5f9d7300dd0b7827a
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-03 15:47:30 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -1,24 +1,29 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n,i,temp,a=1,b=1;
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		for(i=1;i<=n;i++)
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n<=0)
+		System.out.println("invalid");
+		else if(n==1)
+			System.out.println(1);
+		else
 		{
-			System.out.print(a+" ");
-			 temp= a+b;
+			
+		int a=1,b=1,next;
+		System.out.print(a+" "+b);
+		for(int i=2;i<n;i++)
+		{
+			next=a+b;
+			System.out.print(" "+next);
 			a=b;
-			b=temp;
+			b=next;
 		}
-	
+		}
 	}
 }
