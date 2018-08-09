a417dd18b29bd7ebaf29d6e83d2f870653c95ee0
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-09 10:59:38 +0530

    Create 42.java

---
+++ 42.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		int result=a.compareTo(b);
+		if(result==0)  
+		 System.out.println("both are equal");  
+		else if(result> 0)  
+		System.out.println(a);  
+		else  
+		System.out.println(b);  
+	}
+}	
