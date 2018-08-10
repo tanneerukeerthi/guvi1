6644f3c38cd122321f6d932311a79d228ecb89d7
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-10 13:09:09 +0530

    Create 54.java

---
+++ 54.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,m;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		m=n-1;
+		if(m%2==0)
+		System.out.println(m);
+		else
+		System.out.println(n);
+			
+		
+	}
+}
