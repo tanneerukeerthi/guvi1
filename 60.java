f54968d33c2d53005e0c6de04166476eb9412a1e
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-11 13:05:03 +0530

    Create 60.java

---
+++ 60.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,sum=0,i;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>0)
+		{
+			for(i=1;i<=n;i++)
+			{
+				sum=sum+i;
+			}
+			System.out.println(sum);
+		}	
+		else
+		System.out.println("enter positive numbers");
+	}
+}
