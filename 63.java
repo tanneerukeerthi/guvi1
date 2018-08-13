cb586db38e6b820759afd6d3ed18e61be6bc47db
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-13 11:03:25 +0530

    Create 63.java

---
+++ 63.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  int n,min,i;
+		 Scanner s=new Scanner(System.in);
+		 int a[]=new int[20];
+                           n=s.nextInt();
+		 for(i=1;i<=n;i++)
+		 {
+		 	a[i]=s.nextInt();
+		 }
+		 min=a[1];
+		 for(i=1;i<=n;i++)
+		 {
+		 	if(min>a[i])
+		 	{
+		 		min=a[i];
+		 	}
+		 	
+		 }
+		 System.out.println(min);
+
+	}
+}
