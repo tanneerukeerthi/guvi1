c98987b436cdf1d85019492d6c8f704d24152b5a
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-13 12:12:10 +0530

    Create 57.java

---
+++ 57.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,m,i,count=0;
+		int arr[]=new int[20];
+		Scanner sc=new Scanner(System.in);
+		 n=sc.nextInt();
+		 m=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(m==arr[i])
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
