e1cac75886af53cd84ebb52ceb5857f3de02f81d
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-14 10:38:18 +0530

    Create 68.java

---
+++ 68.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n;
+	   Scanner sc=new Scanner(System.in);
+	   n=sc.nextInt();
+	   if(n>0)
+	   {
+	   	if(n%7==0)
+	   	System.out.println("yes");
+	   	else
+	   	System.out.println("no");
+	   }
+	   else
+	   System.out.println("enter positive numbers");
+	}
+}
