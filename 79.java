325d8b7a6b163c4cd2b3fa2a74edfe0fba20b8eb
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-17 10:11:29 +0530

    Update 79.java

--- 79.java
+++ 79.java
@@ -12,37 +12,29 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int m=s.nextInt();
-		int p=n*m;
-		int fl=0;
-		if(n>0&&m>0)
+		int p=n*m;int fl=0;
+		if(n==0 || m==0)
 		{
-			if(n==0 || m==0)
-			{
-		             	System.out.println("yes");
-			}
-			else if(n==m)
-			{
-			       System.out.print("yes");
-			}
-			else
-			{
-			        for(int i=1;i<=p/2;i++)
-			         {
-				if((i*i)==p)
-				{
-				            fl=1;
-			             	break;
-				}
-			         }	
-			}
-		
-			if(fl==1)
+			System.out.println("yes");
+		}
+		else if(n==m)
+		{
 			System.out.print("yes");
-			else
-			System.out.print("no");
 		}
 		else
-		System.out.println("enter positive numbers");
+		{
+		for(int i=1;i<=p/2;i++)
+		{
+			if((i*i)==p)
+			{
+			fl=1;break;
+			}
+		}
+		if(fl==1)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+		}
 		
 	}
 }
