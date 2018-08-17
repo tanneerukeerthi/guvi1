b6a91accd96d764f88048ad81f3da968e73e3c82
Saiyakkala3737 <41730713+Saiyakkala3737@users.noreply.github.com>
2018-08-17 09:40:37 +0530

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				int i,n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>0)
+		{
+			System.out.print(1);
+		
+			for(i=2;i<=n;i++)
+			{
+				if(n%i==0)
+				{
+					System.out.print(" "+i);
+				}
+			}
+		}
+		else
+		System.out.println("enter only positive numbers");
+	}
+}
